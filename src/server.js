888dbc4d6e9d2e8013db55611abd063272557479
minsky922 <minsky922@gmail.com>
2024-05-05 02:39:21 +0900

    socket.io

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { Server } from 'socket.io';
 import http from 'http';
-import WebSocket from 'ws';
+import { instrument } from '@socket.io/admin-ui';
+// import WebSocket from 'ws';
 
 const app = express();
 
@@ -13,8 +15,64 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on: http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://localhost:3000/admin/socket.io"],
+        credentials: true,
+    },
+});
+instrument(wsServer, {
+    auth: false
+});
+
+function publicRooms() {
+    const { sockets: { adapter: { sids, rooms } } } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+// const sids = wsServer.sockets.adapter.sids;
+// const rooms = wsServer.sockets.adapter.rooms;
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anonymous";
+    socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter.rooms);
+        console.log(`Socket Event: ${event}`);
+    });
+
+    // console.log(socket);
+    socket.on("enter_room", (roomName, done) => {
+        // console.log(socket.id);
+        // console.log(socket.rooms); // set { <socket.id> }
+        socket.join(roomName);
+        // console.log(socket.rooms); // set { <socket.id>, roomName }
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // roomName에 있는 모든 socket(사람)들에게 welcome event를 보낸다
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
+
+    });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+});
 
 function onSocketClose() {
     console.log("Disconnected from the Browser ❌");
@@ -24,33 +82,33 @@ function onSocketMessage(message) {
     console.log(message.toString('utf8'));
 }
 
-const sockets_arr = [];
+// const sockets_arr = [];
 
-wss.on("connection", (socket) => {
-    sockets_arr.push(socket);
-    socket["nickname"] = "Anonymous";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg); //Json.parse는 string을 object로 바꿔준다
-        switch (message.type) {
-            case "new_message":
-                sockets_arr.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-            // console.log(message.payload);
-        }
-        // if (parsed.type === "new_message") {
-        //     sockets_arr.forEach((aSocket) => aSocket.send(parsed.payload));
-        // } else if(parsed.type === "nickname") {
-        //     console.log(parsed.payload);
-        // }
-        // sockets_arr.forEach((aSocket) => aSocket.send(message.toString('utf8')));
-    });
-});
+// wss.on("connection", (socket) => {
+//     sockets_arr.push(socket);
+//     socket["nickname"] = "Anonymous";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg); //Json.parse는 string을 object로 바꿔준다
+//         switch (message.type) {
+//             case "new_message":
+//                 sockets_arr.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//                 break;
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//             // console.log(message.payload);
+//         }
+//         // if (parsed.type === "new_message") {
+//         //     sockets_arr.forEach((aSocket) => aSocket.send(parsed.payload));
+//         // } else if(parsed.type === "nickname") {
+//         //     console.log(parsed.payload);
+//         // }
+//         // sockets_arr.forEach((aSocket) => aSocket.send(message.toString('utf8')));
+//     });
+// });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
 
 // {
 //     type: "message",
